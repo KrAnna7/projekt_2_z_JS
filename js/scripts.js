47b14914ef68887e5c9b580dfe8a85f5aff2701e
KrAnna7 <anna.dzioch@gmail.com>
2017-04-08 10:55:18 +0200

    dodanie nowych plików

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+// plik scripts.js
+
+var name = prompt('Wpisz swoje imię');
+//Zawieszenie funki alert
+/*alert('Witaj, ' + name);*/
+//Zawartość zostanie wyświetlona w Console
+console.log('Witaj, ' + name);
